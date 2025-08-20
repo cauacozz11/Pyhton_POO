dd0ce0f0af413c101ea73f5775d1f849dcfc365c
cauacozz11 <cauacozz@gmail.com>
2025-08-20 18:37:32 -0300

    final

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -9,14 +9,14 @@ class Restaurante:
         self._ativo = False
         self._avaliacao = []
         Restaurante.restaurantes.append(self)
-        
+         
     def __str__(self):
         return f"{self._nome} | {self._categoria} | {self.media_avaliacao()}" 
     @classmethod
     def listar_restaurantes(cls):
         print(f"{'Nome'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'.ljust(25)} | Avaliação: ")
         for i in cls.restaurantes: 
-            print(f"{i._nome.ljust(25)} | {i._categoria.ljust(25)} | {i.chiclete.ljust(25)} | {i.media_avaliacao()}")
+            print(f"{i._nome.ljust(25)} | {i._categoria.ljust(25)} | {i.chiclete.ljust(25)} | {i.media_avaliacao}")
             
     @property
     def chiclete(self):
@@ -26,12 +26,13 @@ class Restaurante:
         self._ativo = not self._ativo  
         
     def receber_avaliacao(self, cliente, nota):
-        avaliacao = Avaliacao(cliente, nota)
-        self._avaliacao.append(avaliacao)
-        
+        if 2 < nota <= 5:
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao)
+    @property   
     def media_avaliacao(self):
         if not self._avaliacao:
-            return 0.0
+            return "-"
         soma_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
         quantidade_notas = len(self._avaliacao)
         media = round(soma_notas/quantidade_notas, 1)
